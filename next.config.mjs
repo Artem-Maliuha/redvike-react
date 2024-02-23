c64959c98737ee5d9e4b2f9956b1c31c29cde405
Artem Maliuha <invisbox@gmail.com>
2024-02-23 03:19:00 +0100

    [NOTASK] Remove stock code. Add envs, lint, prettier. Configuring

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    publicRuntimeConfig: {
+        pokemonApiUrl: process.env.POKEMON_API_URL,
+        pokemonLimit: 150,
+        pokemonByPage: 20,
+    },
+    rewrites: () => [{
+        source: '/',
+        destination: '/1'
+    }]
+};
 
 export default nextConfig;
